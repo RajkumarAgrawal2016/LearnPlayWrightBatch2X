f418bd243ab9dd8604e2f5198c871ec8ba8ce48a
Rajkumar Agrawal <raj@example.com>
2026-05-23 16:51:28 +0530

    New files added

---
+++ chapter_07_if_else/49_if_ifelse_if.js
@@ -0,0 +1,25 @@
+let number = 7;
+
+if(number%2===0){
+    console.log("its even number");
+}else
+    console.log("its odd number");
+
+let num= 90;
+let grade = "A";  
+
+if(num = 90){
+    console.log("grade:A");
+}else if(num>=80){
+    console.log("grade: B");
+}else if(num>=70){
+    console.log("grade:C");
+}
+else if(num>=60){
+    console.log("grade: B");
+}else if(num<=60){
+    console.log("Fail");
+}
+
+//leap year check
+
